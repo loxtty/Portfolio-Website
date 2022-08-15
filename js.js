2f011fc709111fe9d27bd601e0f505988c024fd6
Mohammed Thiyab <108061840+loxtty@users.noreply.github.com>
2022-08-15 03:22:27 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,35 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () => {
+    themeToggler.classList.toggle('fa-sun');
+    if (themeToggler.classList.contains('fa-sun')) {
+        document.body.classList.add('active');
+    } else {
+        document.body.classList.remove('active');
+    }
+}
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+    //     reset: true
+});
+
+sr.reveal('.btn , .share, .skills__subtitle, .skills__text', {});
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
+sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
